94d14e1d605c2ddf5847e493a3e152d1c1d43edc
Lukas Zapletal <lzap+git@redhat.com>
2025-06-27 15:11:40 +0200

    feat: wasm importing done

--- cmd/wasm/wasm.go
+++ cmd/wasm/wasm.go
@@ -9,6 +9,7 @@ import (
 	"github.com/BurntSushi/toml"
 	"github.com/osbuild/blueprint-schema/pkg/conv"
 	"github.com/osbuild/blueprint-schema/pkg/parse"
+	"gopkg.in/yaml.v3"
 )
 
 func wasmValidateUBP(this js.Value, p []js.Value) any {
@@ -36,7 +37,7 @@ func wasmExportTOML(this js.Value, p []js.Value) any {
 
 	b, _, err, warn := parse.UnmarshalAny([]byte(p[0].String()))
 	if err != nil {
-		return js.ValueOf([]any{"", fmt.Sprintf("Failed to unmarshal YAML: %v", err)})
+		return js.ValueOf([]any{"", fmt.Sprintf("Failed to unmarshal: %v", err)})
 	}
 	if warn != nil {
 		return js.ValueOf([]any{"", fmt.Sprintf("Unexpected warning(s): %v", err)})
@@ -63,7 +64,7 @@ func wasmExportJSON(this js.Value, p []js.Value) any {
 
 	b, _, err, warn := parse.UnmarshalAny([]byte(p[0].String()))
 	if err != nil {
-		return js.ValueOf([]any{"", fmt.Sprintf("Failed to unmarshal YAML: %v", err)})
+		return js.ValueOf([]any{"", fmt.Sprintf("Failed to unmarshal: %v", err)})
 	}
 	if warn != nil {
 		return js.ValueOf([]any{"", fmt.Sprintf("Unexpected warning(s): %v", err)})
@@ -83,10 +84,66 @@ func wasmExportJSON(this js.Value, p []js.Value) any {
 	return js.ValueOf([]any{string(buf), ""})
 }
 
+func wasmImportYAML(this js.Value, p []js.Value) any {
+	if len(p) != 1 {
+		return js.ValueOf([]any{"", "Import TOML expects exactly one argument (BP string)"})
+	}
+
+	_, b, err, warn := parse.UnmarshalAny([]byte(p[0].String()))
+	if err != nil {
+		return js.ValueOf([]any{"", fmt.Sprintf("Failed to unmarshal: %v", err)})
+	}
+	if warn != nil {
+		js.Global().Get("console").Call("warn", warn.Error())
+	}
+
+	importer := conv.NewInternalImporter(b)
+	result, logs := importer.Import()
+	if logs != nil {
+		js.Global().Get("console").Call("warn", logs.Error())
+	}
+
+	buf, err := yaml.Marshal(result)
+	if err != nil {
+		return js.ValueOf([]any{"", fmt.Sprintf("Failed to marshal YAML: %v", err)})
+	}
+
+	return js.ValueOf([]any{string(buf), ""})
+}
+
+func wasmImportJSON(this js.Value, p []js.Value) any {
+	if len(p) != 1 {
+		return js.ValueOf([]any{"", "Import TOML expects exactly one argument (BP string)"})
+	}
+
+	_, b, err, warn := parse.UnmarshalAny([]byte(p[0].String()))
+	if err != nil {
+		return js.ValueOf([]any{"", fmt.Sprintf("Failed to unmarshal: %v", err)})
+	}
+	if warn != nil {
+		js.Global().Get("console").Call("warn", warn.Error())
+	}
+
+	importer := conv.NewInternalImporter(b)
+	result, logs := importer.Import()
+	if logs != nil {
+		js.Global().Get("console").Call("warn", logs.Error())
+	}
+
+	buf, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return js.ValueOf([]any{"", fmt.Sprintf("Failed to marshal JSON: %v", err)})
+	}
+
+	return js.ValueOf([]any{string(buf), ""})
+}
+
 func main() {
 	js.Global().Set("wasmValidateUBP", js.FuncOf(wasmValidateUBP))
 	js.Global().Set("wasmExportTOML", js.FuncOf(wasmExportTOML))
 	js.Global().Set("wasmExportJSON", js.FuncOf(wasmExportJSON))
+	js.Global().Set("wasmImportYAML", js.FuncOf(wasmImportYAML))
+	js.Global().Set("wasmImportJSON", js.FuncOf(wasmImportJSON))
 
 	// Keep the program running
 	select {}
