83759585367ae438704550968c0110636bf6d11b
Lukas Zapletal <lzap+git@redhat.com>
2025-09-10 14:58:23 +0200

    conv: update convertors for structs without pointers
    Now that the genereated structs do not contain pointers anymore,
    the convertors need to be updated to reflect that.

--- pkg/conv/bp_export.go
+++ pkg/conv/bp_export.go
@@ -45,10 +45,6 @@ func (e *InternalExporter) Export() (*bp.Blueprint, error) {
 }
 
 func (e *InternalExporter) exportPackages() []bp.Package {
-	if e.from.DNF == nil || e.from.DNF.Packages == nil {
-		return nil
-	}
-
 	var dash bool
 	var s []bp.Package
 	for _, pkg := range e.from.DNF.Packages {
@@ -73,10 +69,6 @@ func (e *InternalExporter) exportPackages() []bp.Package {
 }
 
 func (e *InternalExporter) exportGroups() []bp.Group {
-	if e.from.DNF == nil || e.from.DNF.Groups == nil {
-		return nil
-	}
-
 	var s []bp.Group
 	for _, pkg := range e.from.DNF.Groups {
 		s = append(s, bp.Group{
@@ -88,10 +80,6 @@ func (e *InternalExporter) exportGroups() []bp.Group {
 }
 
 func (e *InternalExporter) exportModules() []bp.EnabledModule {
-	if e.from.DNF == nil || e.from.DNF.Modules == nil {
-		return nil
-	}
-
 	var s []bp.EnabledModule
 	for _, pkg := range e.from.DNF.Modules {
 		p := splitStringEmptyN(pkg, ":", 2)
@@ -106,10 +94,6 @@ func (e *InternalExporter) exportModules() []bp.EnabledModule {
 }
 
 func (e *InternalExporter) exportContainers() []bp.Container {
-	if e.from.Containers == nil {
-		return nil
-	}
-
 	var s []bp.Container
 	for _, container := range e.from.Containers {
 		s = append(s, bp.Container{
@@ -141,23 +125,13 @@ func (e *InternalExporter) exportCustomizations() *bp.Customizations {
 	to.Ignition = e.exportIgnition()
 	to.Files, to.Directories = e.exportFSNodes()
 	to.Repositories, to.RPM = e.exportRepositories()
-	if e.from.FIPS != nil {
-		to.FIPS = ptr.ToNilIfEmpty(e.from.FIPS.Enabled)
-	}
+	to.FIPS = ptr.ToNilIfEmpty(e.from.FIPS.Enabled)
 	to.CACerts = e.exportCACerts()
-
-	if reflect.DeepEqual(to, bp.Customizations{}) {
-		return nil // omitzero
-	}
 
 	return &to
 }
 
 func (e *InternalExporter) exportKernel() *bp.KernelCustomization {
-	if e.from.Kernel == nil {
-		return nil
-	}
-
 	to := &bp.KernelCustomization{}
 	to.Name = e.from.Kernel.Package
 	if len(e.from.Kernel.CmdlineAppend) > 0 {
@@ -168,10 +142,6 @@ func (e *InternalExporter) exportKernel() *bp.KernelCustomization {
 }
 
 func (e *InternalExporter) exportUserCustomization() []bp.UserCustomization {
-	if e.from.Accounts == nil || e.from.Accounts.Users == nil {
-		return nil
-	}
-
 	var s []bp.UserCustomization
 	for _, u := range e.from.Accounts.Users {
 		uc := bp.UserCustomization{}
@@ -196,6 +166,7 @@ func (e *InternalExporter) exportUserCustomization() []bp.UserCustomization {
 		if u.Expires != nil {
 			uc.ExpireDate = ptr.ToNilIfEmpty(u.Expires.Days())
 		}
+
 		if u.ForcePasswordChange != nil {
 			uc.ForcePasswordReset = u.ForcePasswordChange
 		}
@@ -207,10 +178,6 @@ func (e *InternalExporter) exportUserCustomization() []bp.UserCustomization {
 }
 
 func (e *InternalExporter) exportGroupCustomization() []bp.GroupCustomization {
-	if e.from.Accounts == nil || e.from.Accounts.Groups == nil {
-		return nil
-	}
-
 	var s []bp.GroupCustomization
 	for _, g := range e.from.Accounts.Groups {
 		gc := bp.GroupCustomization{}
@@ -225,10 +192,6 @@ func (e *InternalExporter) exportGroupCustomization() []bp.GroupCustomization {
 }
 
 func (e *InternalExporter) exportTimezoneCustomization() *bp.TimezoneCustomization {
-	if e.from.Timedate == nil {
-		return nil
-	}
-
 	to := &bp.TimezoneCustomization{}
 	to.Timezone = ptr.ToNilIfEmpty(e.from.Timedate.Timezone)
 	to.NTPServers = e.from.Timedate.NTPServers
@@ -237,10 +200,6 @@ func (e *InternalExporter) exportTimezoneCustomization() *bp.TimezoneCustomizati
 }
 
 func (e *InternalExporter) exportLocaleCustomization() *bp.LocaleCustomization {
-	if e.from.Locale == nil {
-		return nil
-	}
-
 	to := &bp.LocaleCustomization{}
 	if len(e.from.Locale.Keyboards) > 0 {
 		to.Keyboard = ptr.ToNilIfEmpty(e.from.Locale.Keyboards[0])
@@ -254,10 +213,6 @@ func (e *InternalExporter) exportLocaleCustomization() *bp.LocaleCustomization {
 }
 
 func (e *InternalExporter) exportFirewallCustomization() *bp.FirewallCustomization {
-	if e.from.Network == nil || e.from.Network.Firewall == nil || len(e.from.Network.Firewall.Services) == 0 {
-		return nil
-	}
-
 	to := &bp.FirewallCustomization{
 		Services: &bp.FirewallServicesCustomization{},
 	}
@@ -301,10 +256,6 @@ func (e *InternalExporter) exportFirewallCustomization() *bp.FirewallCustomizati
 }
 
 func (e *InternalExporter) exportSystemdCustomization() *bp.ServicesCustomization {
-	if e.from.Systemd == nil {
-		return nil
-	}
-
 	to := &bp.ServicesCustomization{}
 	to.Enabled = e.from.Systemd.Enabled
 	to.Disabled = e.from.Systemd.Disabled
@@ -314,10 +265,6 @@ func (e *InternalExporter) exportSystemdCustomization() *bp.ServicesCustomizatio
 }
 
 func (e *InternalExporter) exportStorage() *bp.DiskCustomization {
-	if e.from.Storage == nil {
-		return nil
-	}
-
 	to := &bp.DiskCustomization{}
 	to.Type = e.from.Storage.Type.String()
 	to.MinSize = e.from.Storage.Minsize.Bytes()
@@ -385,121 +332,97 @@ func (e *InternalExporter) exportStorage() *bp.DiskCustomization {
 		}
 	}
 
-	if to.Type == "" && to.MinSize == 0 && len(to.Partitions) == 0 {
-		return nil
-	}
-
 	return to
 }
 
 func (e *InternalExporter) exportInstaller() (string, *bp.InstallerCustomization) {
 	var installationDevice string
-	if e.from.Installer == nil {
-		return installationDevice, nil
+	to := &bp.InstallerCustomization{
+		Modules: &bp.AnacondaModules{},
+	}
+	to.Unattended = e.from.Installer.Anaconda.Unattended
+	to.SudoNopasswd = e.from.Installer.Anaconda.SudoNOPASSWD
+	if e.from.Installer.Anaconda.Kickstart != "" {
+		to.Kickstart = &bp.Kickstart{Contents: e.from.Installer.Anaconda.Kickstart}
 	}
 
-	var to *bp.InstallerCustomization
-	if e.from.Installer.Anaconda != nil {
-		to = &bp.InstallerCustomization{
-			Modules: &bp.AnacondaModules{},
-		}
-		to.Unattended = e.from.Installer.Anaconda.Unattended
-		to.SudoNopasswd = e.from.Installer.Anaconda.SudoNOPASSWD
-		if e.from.Installer.Anaconda.Kickstart != "" {
-			to.Kickstart = &bp.Kickstart{Contents: e.from.Installer.Anaconda.Kickstart}
-		}
-
-		if len(e.from.Installer.Anaconda.EnabledModules) > 0 {
-			for _, module := range e.from.Installer.Anaconda.EnabledModules {
-				to.Modules.Enable = append(to.Modules.Enable, string(module))
-			}
-		}
-
-		if len(e.from.Installer.Anaconda.DisabledModules) > 0 {
-			for _, module := range e.from.Installer.Anaconda.DisabledModules {
-				to.Modules.Disable = append(to.Modules.Disable, string(module))
-			}
+	if len(e.from.Installer.Anaconda.EnabledModules) > 0 {
+		for _, module := range e.from.Installer.Anaconda.EnabledModules {
+			to.Modules.Enable = append(to.Modules.Enable, string(module))
 		}
 	}
 
-	if e.from.Installer.CoreOS != nil {
-		installationDevice = e.from.Installer.CoreOS.InstallationDevice
+	if len(e.from.Installer.Anaconda.DisabledModules) > 0 {
+		for _, module := range e.from.Installer.Anaconda.DisabledModules {
+			to.Modules.Disable = append(to.Modules.Disable, string(module))
+		}
 	}
+
+	installationDevice = e.from.Installer.CoreOS.InstallationDevice
 
 	return installationDevice, to
 }
 
 func (e *InternalExporter) exportRegistration() (*bp.RHSMCustomization, *bp.FDOCustomization) {
-	if e.from.Registration == nil {
-		return nil, nil
-	}
 	r := e.from.Registration
 
-	var fdo *bp.FDOCustomization
-	if r.RegistrationFDO != nil {
-		fdo = &bp.FDOCustomization{}
-		fdo.DiMfgStringTypeMacIface = r.RegistrationFDO.DiMfgStringTypeMacIface
-		fdo.DiunPubKeyHash = r.RegistrationFDO.DiunPubKeyHash
-		fdo.DiunPubKeyInsecure = strconv.FormatBool(r.RegistrationFDO.DiunPubKeyInsecure)
-		fdo.DiunPubKeyRootCerts = r.RegistrationFDO.DiunPubKeyRootCerts
-		fdo.ManufacturingServerURL = r.RegistrationFDO.ManufacturingServerURL
-	}
-
-	var rhsm *bp.RHSMCustomization
-	if r.RegistrationRedHat != nil && r.RegistrationRedHat.RegistrationRHSM != nil {
-		rhsm = &bp.RHSMCustomization{
-			Config: &bp.RHSMConfig{
-				SubscriptionManager: &bp.SubManConfig{
-					RHSMConfig: &bp.SubManRHSMConfig{
-						ManageRepos:          r.RegistrationRedHat.RegistrationRHSM.RepositoryManagement,
-						AutoEnableYumPlugins: r.RegistrationRedHat.RegistrationRHSM.AutoEnable,
-					},
-					RHSMCertdConfig: &bp.SubManRHSMCertdConfig{
-						AutoRegistration: r.RegistrationRedHat.RegistrationRHSM.AutoRegistration,
-					},
-				},
-				DNFPlugins: &bp.SubManDNFPluginsConfig{
-					ProductID: &bp.DNFPluginConfig{
-						Enabled: r.RegistrationRedHat.RegistrationRHSM.ProductPluginEnabled,
-					},
-					SubscriptionManager: &bp.DNFPluginConfig{
-						Enabled: r.RegistrationRedHat.RegistrationRHSM.Enabled,
-					},
-				},
-			},
-		}
+	if !reflect.DeepEqual(r, ubp.Registration{}) {
 		e.log.Println("registration not converted")
 	}
 
-	if rhsm != nil {
-		emptyRHSM := &bp.RHSMCustomization{
-			Config: &bp.RHSMConfig{
-				DNFPlugins: &bp.SubManDNFPluginsConfig{
-					ProductID:           &bp.DNFPluginConfig{},
-					SubscriptionManager: &bp.DNFPluginConfig{},
+	fdo := &bp.FDOCustomization{}
+	fdo.DiMfgStringTypeMacIface = r.RegistrationFDO.DiMfgStringTypeMacIface
+	fdo.DiunPubKeyHash = r.RegistrationFDO.DiunPubKeyHash
+	fdo.DiunPubKeyInsecure = strconv.FormatBool(r.RegistrationFDO.DiunPubKeyInsecure)
+	fdo.DiunPubKeyRootCerts = r.RegistrationFDO.DiunPubKeyRootCerts
+	fdo.ManufacturingServerURL = r.RegistrationFDO.ManufacturingServerURL
+
+	rhsm := &bp.RHSMCustomization{
+		Config: &bp.RHSMConfig{
+			SubscriptionManager: &bp.SubManConfig{
+				RHSMConfig: &bp.SubManRHSMConfig{
+					ManageRepos:          r.RegistrationRedHat.RegistrationRHSM.RepositoryManagement,
+					AutoEnableYumPlugins: r.RegistrationRedHat.RegistrationRHSM.AutoEnable,
 				},
-				SubscriptionManager: &bp.SubManConfig{
-					RHSMConfig:      &bp.SubManRHSMConfig{},
-					RHSMCertdConfig: &bp.SubManRHSMCertdConfig{},
+				RHSMCertdConfig: &bp.SubManRHSMCertdConfig{
+					AutoRegistration: r.RegistrationRedHat.RegistrationRHSM.AutoRegistration,
 				},
 			},
-		}
-		if reflect.DeepEqual(rhsm.Config.DNFPlugins, emptyRHSM.Config.DNFPlugins) {
-			rhsm.Config.DNFPlugins = nil // omitzero
-		}
-		if reflect.DeepEqual(rhsm.Config.SubscriptionManager, emptyRHSM.Config.SubscriptionManager) {
-			rhsm.Config.SubscriptionManager = nil // omitzero
-		}
+			DNFPlugins: &bp.SubManDNFPluginsConfig{
+				ProductID: &bp.DNFPluginConfig{
+					Enabled: r.RegistrationRedHat.RegistrationRHSM.ProductPluginEnabled,
+				},
+				SubscriptionManager: &bp.DNFPluginConfig{
+					Enabled: r.RegistrationRedHat.RegistrationRHSM.Enabled,
+				},
+			},
+		},
+	}
+
+	emptyRHSM := &bp.RHSMCustomization{
+		Config: &bp.RHSMConfig{
+			DNFPlugins: &bp.SubManDNFPluginsConfig{
+				ProductID:           &bp.DNFPluginConfig{},
+				SubscriptionManager: &bp.DNFPluginConfig{},
+			},
+			SubscriptionManager: &bp.SubManConfig{
+				RHSMConfig:      &bp.SubManRHSMConfig{},
+				RHSMCertdConfig: &bp.SubManRHSMCertdConfig{},
+			},
+		},
+	}
+
+	if reflect.DeepEqual(rhsm.Config.DNFPlugins, emptyRHSM.Config.DNFPlugins) {
+		rhsm.Config.DNFPlugins = nil // omitzero
+	}
+	if reflect.DeepEqual(rhsm.Config.SubscriptionManager, emptyRHSM.Config.SubscriptionManager) {
+		rhsm.Config.SubscriptionManager = nil // omitzero
 	}
 
 	return ptr.EmptyToNil(rhsm), ptr.EmptyToNil(fdo)
 }
 
 func (e *InternalExporter) exportOpenSCAP() *bp.OpenSCAPCustomization {
-	if e.from.OpenSCAP == nil {
-		return nil
-	}
-
 	to := &bp.OpenSCAPCustomization{}
 	to.DataStream = e.from.OpenSCAP.Datastream
 	to.ProfileID = e.from.OpenSCAP.ProfileID
@@ -529,11 +452,7 @@ func (e *InternalExporter) exportOpenSCAP() *bp.OpenSCAPCustomization {
 }
 
 func (e *InternalExporter) exportIgnition() *bp.IgnitionCustomization {
-	if e.from.Ignition == nil {
-		return nil
-	}
-
-	to := &bp.IgnitionCustomization{}
+	to := bp.IgnitionCustomization{}
 	iu, ie, err := e.from.Ignition.SelectUnion()
 	if err != nil {
 		e.log.Printf("could not parse ignition: %v", err)
@@ -546,11 +465,9 @@ func (e *InternalExporter) exportIgnition() *bp.IgnitionCustomization {
 		to.FirstBoot = &bp.FirstBootIgnitionCustomization{
 			ProvisioningURL: iu.URL,
 		}
-	} else {
-		e.log.Printf("could not parse ignition: %v", err)
 	}
 
-	return ptr.EmptyToNil(to)
+	return &to
 }
 
 func (e *InternalExporter) exportFSNodes() ([]bp.FileCustomization, []bp.DirectoryCustomization) {
@@ -612,10 +529,6 @@ func (e *InternalExporter) exportFSNodes() ([]bp.FileCustomization, []bp.Directo
 }
 
 func (e *InternalExporter) exportRepositories() ([]bp.RepositoryCustomization, *bp.RPMCustomization) {
-	if e.from.DNF == nil {
-		return nil, nil
-	}
-
 	var repos []bp.RepositoryCustomization
 	for _, repo := range e.from.DNF.Repositories {
 		burl, bmeta, bmirror, err := repo.Source.SelectUnion()
@@ -625,10 +538,8 @@ func (e *InternalExporter) exportRepositories() ([]bp.RepositoryCustomization, *
 		}
 
 		var configure, install *bool
-		if repo.Usage != nil {
-			configure = repo.Usage.Configure
-			install = repo.Usage.Install
-		}
+		configure = repo.Usage.Configure
+		install = repo.Usage.Install
 
 		repos = append(repos, bp.RepositoryCustomization{
 			Id:             repo.ID,
